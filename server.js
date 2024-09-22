87337a283a88f60c643afe8ab4f6a425f0291166
dorcasChero <dorcas@sendyit.com>
2024-09-22 22:13:47 +0300

    add server url

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ let index = require('./routes/index');
 let image = require('./routes/image');
 
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
+let mongodb_url = 'mongodb+srv://dorcasmosonik90:VgsnizzIXdKhvYqX@gallerycluster.9fp3b.mongodb.net/?retryWrites=true&w=majority&appName=GalleryCluster';
 let dbName = 'darkroom';
-mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
+mongoose.connect(`${mongodb_url}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
 });
 
