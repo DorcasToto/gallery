6ce1266921e55b65dadae3aa2fe0f7860193a126
dorcasChero <dorcas@sendyit.com>
2024-09-23 00:15:22 +0300

    modify config file

--- _config.js
+++ _config.js
@@ -2,10 +2,8 @@ var config = {};
 
 // Corrected connection strings without angle brackets
 config.mongoURI = {
-  production: 'mongodb+srv://dorcasmosonik90:VgsnizzIXdKhvYqX@gallerycluster.9fp3b.mongodb.net/?retryWrites=true&w=majority',
-  development: 'mongodb+srv://dorcasmosonik90:VgsnizzIXdKhvYqX@gallerycluster.9fp3b.mongodb.net/?retryWrites=true&w=majority',
-  test: 'mongodb+srv://dorcasmosonik90:VgsnizzIXdKhvYqX@gallerycluster.9fp3b.mongodb.net/?retryWrites=true&w=majority',
+  production: 'mongodb+srv://dorcasmosonik90:VgsnizzIXdKhvYqX@gallerycluster.9fp3b.mongodb.net/?retryWrites=true&w=majority&appName=GalleryCluster&ssl=true',
+  development: 'mongodb+srv://dorcasmosonik90:VgsnizzIXdKhvYqX@gallerycluster.9fp3b.mongodb.net/?retryWrites=true&w=majority&appName=GalleryCluster&ssl=true',
+  test: 'mongodb+srv://dorcasmosonik90:VgsnizzIXdKhvYqX@gallerycluster.9fp3b.mongodb.net/?retryWrites=true&w=majority&appName=GalleryCluster&ssl=true',
 };
 module.exports = config;
-
-
