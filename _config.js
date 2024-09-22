0232e578d46ad2992e81eb968dbe20169f36d06f
dorcasChero <dorcas@sendyit.com>
2024-09-22 23:08:20 +0300

    change config file

--- _config.js
+++ _config.js
@@ -2,9 +2,9 @@ var config = {};
 
 // Corrected connection strings without angle brackets
 config.mongoURI = {
-  production: 'mongodb+srv://dorcasmosonik90:VgsnizzIXdKhvYqX@gallerycluster.9fp3b.mongodb.net/?retryWrites=true&w=majority&appName=GalleryCluster',
-  development: 'mongodb+srv://dorcasmosonik90:VgsnizzIXdKhvYqX@gallerycluster.9fp3b.mongodb.net/?retryWrites=true&w=majority&appName=GalleryCluster',
-  test: 'mongodb+srv://dorcasmosonik90:VgsnizzIXdKhvYqX@gallerycluster.9fp3b.mongodb.net/?retryWrites=true&w=majority&appName=GalleryCluster',
+  production: 'mongodb+srv://dorcasmosonik90:VgsnizzIXdKhvYqX@gallerycluster.9fp3b.mongodb.net/?retryWrites=true&w=majority',
+  development: 'mongodb+srv://dorcasmosonik90:VgsnizzIXdKhvYqX@gallerycluster.9fp3b.mongodb.net/?retryWrites=true&w=majority',
+  test: 'mongodb+srv://dorcasmosonik90:VgsnizzIXdKhvYqX@gallerycluster.9fp3b.mongodb.net/?retryWrites=true&w=majority',
 };
 module.exports = config;
 
